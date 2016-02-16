04b86e50002b614d392daee72d0964897704950e
Alex Berger <alex@more-onion.com>
2016-02-16 16:38:06 +0100

    first simple test

---
+++ test/cookiebar_test.js
@@ -0,0 +1,16 @@
+/* global require describe it */
+
+var chai = require('chai');
+var assert = chai.assert;
+var Cookiebar = require('../src/cookiebar');
+
+describe('Cookiebar', function () {
+    describe('constructor', function () {
+        it('should be constructable with new', function () {
+            var cookiebar = new Cookiebar();
+            assert.ok(cookiebar instanceof Cookiebar);
+        });
+    });
+});
+
+// vim: set et ts=4 sw=4 :
